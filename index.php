c51da8458d2bec6937da299a20cd8070d4cdca9f
Sergey <bashkin2013@mail.ru>
2020-05-09 23:12:41 +0400

    feat: added some main functions

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 require_once "config.php";
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+date_default_timezone_set('UTC');
 
 function getFormData(){
   return json_decode(file_get_contents("php://input"), true);
+}
+
+function createPDOConfig() {
+  return "mysql:host=".DBHOST.";dbname=".DBNAME;
+}
+
+function createResponse($response, $code) {
+  switch ($code) {
+    case 200:
+      header("Code: 200 Ok");
+      break;
+    case 400:
+      header("Code: 400 Bad Request");
+      break;
+    case 404:
+      header("Code: 404 Not found");
+      break;
+    default:
+      break;
+  }
+  echo json_encode($response, JSON_UNESCAPED_UNICODE);
 }
 
 header('Access-Control-Allow-Origin: *');
