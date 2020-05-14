30e11c5420e31d100a8d6b551c263342e7e23a86
Sergey <bashkin2013@mail.ru>
2020-05-14 18:25:30 +0400

    feat: added method for get information about categories

---
+++ routes/getCategories.php
@@ -0,0 +1,18 @@
+<?php
+
+if ($method != "GET") {
+  $response['Error'] = 'Not enough data';
+  $code = 400;
+} else {
+  $sql = "SELECT `id_category`, `name_category`, `picture_category` FROM `category-words`";
+  $dbRequest = $db -> prepare($sql);
+  if ($dbRequest -> execute(array($formData['userId']))) {
+    $response = $dbRequest -> fetchAll(PDO::FETCH_ASSOC);
+    $code = 200;
+  } else {
+    $response['Error'] = 'Data base error';
+    $code = 400;
+  }
+}
+
+?>
