866dd927866bbcd3ae39b2b784b68ef608c673ac
Sergey <bashkin2013@mail.ru>
2020-05-25 21:29:14 +0400

    feat: added API method for determine category ID

---
+++ routes/determineCategoryId.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($method != "GET" &&
+  isset($formData['categoryName'])
+) {
+  $response['Error'] = 'Not enough data';
+  $code = 400;
+} else {
+  $sql = "SELECT `id_category` FROM `category-words` WHERE `name_category` = ?";
+  $dbRequest = $db -> prepare($sql);
+  if ($dbRequest -> execute(array($formData['categoryName']))) {
+    $dbResponse = $dbRequest -> fetchAll(PDO::FETCH_ASSOC);
+    if (count($dbResponse)) {
+      $response['categoryId'] = $dbResponse[0]['id_category'];
+      $code = 200;
+    } else {
+      $response['Error'] = 'Category not found';
+      $code = 404;
+    }
+  } else {
+    $response['Error'] = 'Data base error';
+    $code = 400;
+  }
+}
+
+?>
